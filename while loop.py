18bc8807467c96b3a6b341a1178547eb8215a447
Sanju-1001011 <155014267+Sanju-1001011@users.noreply.github.com>
2024-07-14 20:53:45 +0545

    Add files via upload

---
+++ while loop.py
@@ -0,0 +1,10 @@
+ # while loop 
+
+appended_numbers =[]
+user_input = input("Enter number :")
+
+while user_input.indigit():
+    user_input("Enter number :")
+    appended_numbers.append(user_input)
+
+print(appended_numbers)
