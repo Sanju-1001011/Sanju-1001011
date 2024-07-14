e1301358253c01de3086482f3032b56ac7c00cf1
Sanju-1001011 <155014267+Sanju-1001011@users.noreply.github.com>
2024-07-14 20:54:11 +0545

    Add files via upload

---
+++ vowel and cons letter.py
@@ -0,0 +1,7 @@
+
+vowel_letter=["a","e","i","o","u"]
+letters = input('Enter the letter: ')
+if letters in vowel_letter:
+    print("It is vowel letter :")
+else:
+    print("It is consonent letter:")
